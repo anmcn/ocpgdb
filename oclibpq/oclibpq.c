fc3f33200e277c04ddb00aa9604e2fdb0e8f1f72
andrewm <andrewm@f649c392-c153-0410-a708-3dafa0ef0028>
2007-06-26 08:10:48 +0000

    Fix whitespace.

--- oclibpq/oclibpq.c
+++ oclibpq/oclibpq.c
@@ -1,3 +1,5 @@
+/* vi:set sw=8 ts=8 noet showmode ai: */
+
 #include "oclibpq.h"
 
 static PyObject *
@@ -5,21 +7,21 @@ unescape(PyPgConnection *self, PyObject *str)
 {
 	unsigned char *escaped, *unescaped;
 	size_t len;
-        PyObject *result;
+	PyObject *result;
 
 	escaped = (unsigned char *)PyString_AsString(str);
 	if (escaped == NULL)
 		return NULL;
 	unescaped = PQunescapeBytea(escaped, &len);
-        result = PyString_FromStringAndSize((char *)unescaped, len);
-        PQfreemem(unescaped);
-        return result;
+	result = PyString_FromStringAndSize((char *)unescaped, len);
+	PQfreemem(unescaped);
+	return result;
 }
 
 static PyMethodDef OCPQMethods[] = {
 	{"unescape", (PyCFunction)unescape, METH_O,
 		PyDoc_STR("Unescape a bytea")},
-        {NULL, NULL, 0, NULL}        /* Sentinel */
+	{NULL, NULL, 0, NULL}	     /* Sentinel */
 };
 
 static char *OCPQ_doco = "XXX module docstring";
@@ -28,17 +30,17 @@ static char *OCPQ_doco = "XXX module docstring";
 PyMODINIT_FUNC
 initoclibpq(void)
 {
-    PyObject *module;
+	PyObject *module;
 
-    module = Py_InitModule4("oclibpq", OCPQMethods, OCPQ_doco,
-                            (PyObject*)NULL, PYTHON_API_VERSION);
-    if (module == NULL)
-	return;
+	module = Py_InitModule4("oclibpq", OCPQMethods, OCPQ_doco,
+				(PyObject*)NULL, PYTHON_API_VERSION);
+	if (module == NULL)
+		return;
 
-    pg_constants_init(module);
-    pg_bytea_init(module);
-    pg_exception_init(module);
-    pg_cell_init(module);
-    pg_result_init(module);
-    pg_connection_init(module);
+	pg_constants_init(module);
+	pg_bytea_init(module);
+	pg_exception_init(module);
+	pg_cell_init(module);
+	pg_result_init(module);
+	pg_connection_init(module);
 }
